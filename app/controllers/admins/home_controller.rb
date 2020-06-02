85cf21e39f77ddb0e169c529fd9b1662605062cc
vagrant <cebasypia.tskm8@gmail.com>
2020-06-02 14:16:31 +0000

    Generate devise users & admins

---
+++ app/controllers/admins/home_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Admins::HomeController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_scope :admin do
+    get 'admins/home' => 'admins/home#index', as: 'admins_home'
+  end
+  devise_for :users
   get 'home/index'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
